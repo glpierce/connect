897ddbb7068395c695ac3bda39aaf550c90d9947
Miguel <miguel.f.c.ayala@gmail.com>
2022-12-06 23:31:03 -0800

    moved db init to a new file so that we avoid circular import, models now based on db.Model isntad to get query

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from __init__ import db
 
-# Set up the base class for the model
-Base = declarative_base()
 
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -16,7 +15,7 @@ class User(Base):
     friends = relationship("Friend", back_populates="user")
 
 
-class Friend(Base):
+class Friend(db.Model):
     __tablename__ = 'friends'
 
     id = Column(Integer, primary_key=True)
