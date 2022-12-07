24c81e86068be665d39aff6a89a433a8507eb3b2
yhindy <yhindy@stanford.edu>
2022-12-06 22:27:59 -0800

    Update backend

--- src/backend/server.py
+++ src/backend/server.py
@@ -15,9 +15,22 @@ def me():
     # TODO(Yousef): implement
     pass
 
+# Account Creation.
+@app.route('/create_account', methods=['POST']))
+def create_account():
+    pass
 
-@app.route("/friends")
-def friends():
+@app.route('/login', methods=['POST']))
+def login():
+    pass
+
+# Accessing/changing friend data.
+@app.route('/add_friend', methods=['POST'])
+def add_friend():
+    pass
+
+@app.route("/get_friends")
+def get_friends():
     data = [
         {"name": "Yousef", "frequency": "Daily"},
         {"name": "Grant", "frequency": "Monthly"},
@@ -26,6 +39,11 @@ def friends():
     print(data)
 
     return jsonify(data)
+
+@app.route('/update_last_messaged', methods=['POST']))
+def update_last_messaged():
+    pass
+
 
 
 if __name__ == "__main__":
