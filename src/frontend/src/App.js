5ddad3bfb2114a346c6bf1affda5018a497dc478
yhindy <yhindy@stanford.edu>
2022-12-06 21:46:03 -0800

    Update app js with starter code

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      friends: [
+        { name: 'Alice' },
+        { name: 'Bob' },
+        { name: 'Carol' },
+        { name: 'Dave' },
+        { name: 'Emily' }
+      ]
+    };
+  }
+
+  handleRemindClick(friend) {
+    alert(`Reminder: Reach out to ${friend.name}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Friends</h1>
+        <ul>
+          {this.state.friends.map(friend => (
+            <li key={friend.name}>
+              {friend.name}
+              <button onClick={() => this.handleRemindClick(friend)}>
+                Remind
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
