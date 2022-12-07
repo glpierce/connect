e39902f15038a6222a372e9fa962d4e7f5037471
yhindy <yhindy@stanford.edu>
2022-12-06 22:20:48 -0800

    Update backend

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-if __name__ == '__main__':
+
+@app.route("/me")
+def me():
+    # TODO(Yousef): implement
+    pass
+
+
+@app.route("/friends")
+def friends():
+    data = [
+        {"name": "Yousef", "frequency": "Daily"},
+        {"name": "Grant", "frequency": "Monthly"},
+        {"name": "Miguel", "frequency": "Yearly"},
+    ]
+    print(data)
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
     app.run()
-
